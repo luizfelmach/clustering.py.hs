ccf96e54ea795c8f03701e1581db4809e8106bc3
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-07-26 13:50:19 -0300

    wip

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,7 +1,58 @@
-import System.IO
-import Data.List (elemIndex, maximumBy)
+import System.IO (hFlush, stdout)
+import Data.List (minimumBy, maximumBy)
+import Data.Ord (comparing)
 
--- Função para dividir string por vírgula
+type Point = (Int, [Double])
+type Path = [Point]
+type Cluster = [Path]
+
+euclidean :: [Double] -> [Double] -> Double
+euclidean v1 v2 = sqrt $ sum $ zipWith (\a b -> (a - b) ^ 2) v1 v2
+
+nearest :: Point -> [Point] -> Point
+nearest target points = minimumBy compareDistance points
+    where
+    compareDistance p1 p2 = compare (distance target p1) (distance target p2)
+    distance (_, coords1) (_, coords2) = euclidean coords1 coords2
+
+path :: [Point] -> Path
+path [] = []
+path [p] = [p]
+path (p:ps) = go p ps
+  where
+    go current [] = [current]
+    go current rest = current : go next newRest
+      where
+        next = nearest current rest
+        newRest = filter (/= next) rest
+
+maxLink :: Path -> (Double, Int)
+maxLink ps = maximumBy (comparing fst) distances
+    where
+    distances = [ (euclidean c1 c2, i)
+                | ((_, c1), (_, c2), i) <- zip3 ps (drop 1 ps) [1..] ]
+
+splitPath :: Path -> (Path, Path)
+splitPath p = splitAt idx p
+    where (_, idx) = maxLink p
+
+selectMaxLink :: Cluster -> (Path, Cluster)
+selectMaxLink cs = (maxPath, others)
+    where
+    maxPath = maximumBy (comparing (fst . maxLink)) cs
+    others = filter (/= maxPath) cs
+
+clustering :: Cluster -> Cluster
+clustering cluster = newPaths ++ rest
+  where
+    (targetPath, rest) = selectMaxLink cluster
+    (p1, p2) = splitPath targetPath
+    newPaths = filter (not . null) [p1, p2]
+
+clusteringK :: Int -> Cluster -> Cluster
+clusteringK 1 cs = cs
+clusteringK n cs = clusteringK (n - 1) (clustering cs)
+
 splitBy :: Char -> String -> [String]
 splitBy _ [] = []
 splitBy delimiter str =
@@ -9,78 +60,21 @@ splitBy delimiter str =
         (before, []) -> [before]
         (before, _:after) -> before : splitBy delimiter after
 
--- Função para calcular distância euclidiana
-euclidean :: [Double] -> [Double] -> Double
-euclidean v1 v2 = sqrt $ sum $ zipWith (\a b -> (a - b) ** 2) v1 v2
-
--- Função para ler vetores do arquivo
-readVectors :: String -> IO [[Double]]
-readVectors filename = do
+readPoints :: String -> IO [Point]
+readPoints filename = do
     content <- readFile filename
-    return (map (map read . splitBy ',') (lines content))
+    let rawVectors = map (map read . splitBy ',') (lines content)
+    return (zip [1..] rawVectors)
 
--- Função para encontrar o próximo vértice não visitado mais próximo
-findNearest :: [[Double]] -> [Bool] -> [Double] -> Int
-findNearest vectors visited current =
-    snd $ minimum [(euclidean current (vectors !! i), i) | (i, vis) <- zip [0..] visited, not vis]
+clustersToString :: Cluster -> String
+clustersToString clusters = unlines (map clusterLine clusters)
 
--- Função para construir o caminho inicial usando nearest neighbor
-buildPath :: [[Double]] -> ([Int], [(Int, Int, Double)])
-buildPath vectors = buildPath' [0] (True : replicate (length vectors - 1) False) []
-  where
-    buildPath' path visited distances
-        | all id visited = (path, distances)
-        | otherwise =
-            case findNearest vectors visited (vectors !! last path) of
-                nextIdx -> buildPath'
-                    (path ++ [nextIdx])
-                    (take nextIdx visited ++ [True] ++ drop (nextIdx + 1) visited)
-                    (distances ++ [(last path, nextIdx, euclidean (vectors !! last path) (vectors !! nextIdx))])
-
--- Função para encontrar a aresta com maior distância
-findMaxEdge :: [Int] -> [(Int, Int, Double)] -> (Int, Int, Double)
-findMaxEdge cluster distances =
-    maximumBy (\(_, _, d1) (_, _, d2) -> compare d1 d2)
-    [edge | edge@(v1, v2, _) <- distances,
-     v1 `elem` cluster && v2 `elem` cluster &&
-     abs (fromJust (elemIndex v1 cluster) - fromJust (elemIndex v2 cluster)) == 1]
-  where
-    fromJust (Just x) = x
-    fromJust Nothing = error "Element not found"
-
--- Função para dividir cluster na aresta de maior distância
-splitCluster :: [Int] -> [(Int, Int, Double)] -> ([Int], [Int])
-splitCluster cluster distances =
-    case findMaxEdge cluster distances of
-        (v1, v2, _) ->
-            case (elemIndex v1 cluster, elemIndex v2 cluster) of
-                (Just i1, Just i2) ->
-                    case (min i1 i2, max i1 i2) of
-                        (minIdx, maxIdx) -> (take (minIdx + 1) cluster, drop maxIdx cluster)
-
--- Função para criar k clusters
-createClusters :: Int -> [Int] -> [(Int, Int, Double)] -> [[Int]]
-createClusters k path distances = createClusters' (k - 1) [path]
-  where
-    createClusters' 0 clusters = clusters
-    createClusters' remaining clusters =
-        case maximumBy (\c1 c2 -> compare (maxEdgeDist c1) (maxEdgeDist c2)) clusters of
-            targetCluster ->
-                case splitCluster targetCluster distances of
-                    (part1, part2) -> createClusters' (remaining - 1)
-                        (part1 : part2 : filter (/= targetCluster) clusters)
-      where
-        maxEdgeDist cluster =
-            case findMaxEdge cluster distances of
-                (_, _, dist) -> dist
-
--- Função para converter índices para string (1-indexado)
-clusterToString :: [Int] -> String
-clusterToString cluster = intercalate ", " (map (show . (+1)) cluster)
+clusterLine :: Path -> String
+clusterLine path = intercalate ", " (map (show . fst) path)
 
 intercalate :: [a] -> [[a]] -> [a]
-intercalate _ [] = []
-intercalate _ [x] = x
+intercalate _ []     = []
+intercalate _ [x]    = x
 intercalate sep (x:xs) = x ++ sep ++ intercalate sep xs
 
 main :: IO ()
@@ -97,12 +91,9 @@ main = do
     hFlush stdout
     kStr <- getLine
 
-    vectors <- readVectors filein
+    points <- readPoints filein
+    let result = clustersToString $ clusteringK (read kStr :: Int) [path points]
 
-    case buildPath vectors of
-        (path, distances) ->
-            case createClusters (read kStr :: Int) path distances of
-                clusters -> do
-                    putStrLn "Agrupamentos:"
-                    mapM_ (\cluster -> putStrLn (clusterToString cluster)) clusters
-                    writeFile fileout (unlines (map clusterToString clusters))
+    putStr "Agrupamentos:\n"
+    putStr result
+    writeFile fileout result
